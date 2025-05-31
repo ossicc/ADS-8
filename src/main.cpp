cce66e66ece2ec5b00ccc7d2815765610635fe63
ossicc <ossic8881@gmail.com>
2025-05-31 15:05:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 
+#include "bst.h"
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
+  const char* filename = "src/war_peace.txt";
+  BST<std::string> tree;
+  makeTree(tree, filename);
+  printFreq(tree);
+  return 0;
 }
